ce929f3923680bf43dca555747b2a6b4b3592372
KaiioBr <kaiomae8@gmail.com>
2026-02-25 19:37:10 +0000

    save

---
+++ Aula 02/Vetor.py
@@ -0,0 +1,30 @@
+import random
+numeros = [13,14,58,66,78,11,5]
+nomes = ["Albana", "Zumira", "Diograce", "zuleide", "Almira"]
+
+# Append - Insere na ultima posiçao -       Exp: numeros.append(9)
+
+# Insert - insere na posiçao desejada -     Exp: numeros.insert(2,71)
+
+# pop - deleta a posiçao do vetor -     Exp: numeros.pop(0)
+
+# remove - Deleta o valor encontrado no vetor -     Exp: numeros.remove(66)
+
+# len - Contagem de valores dentro de um vetor      Exp: Quantidade = len(numeros)
+
+# sort - ORdena de forma crescente     Exp: numeros.sort()
+
+# reverse - Ordena de forma decrescente    Exp: numeros.reverse()
+
+# max - procura o maior valor     Exp: maior = max(numeros)
+
+# min - procura o menor valor      Exp: menor = min(numeros)
+
+# sun - Soma todos os valores dentro de vetor      Exp: total = sum(numeros)
+
+# random.choice - 
+
+
+sorte = random.choice(numeros)
+
+print(f"O valor final da comra e:  {sorte}")
